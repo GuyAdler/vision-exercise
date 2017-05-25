b598d88e70e36d2eaf3f0edd772dcf7ba16c7692
GuyAdler <krynnish.conspiracy@gmail.com>
2017-05-25 23:34:49 +0300

    added q3

--- HW1.py
+++ HW1.py
@@ -1,26 +1,35 @@
 import numpy as np
 import scipy.signal as scisig
+import scipy.ndimage.filters as scifilt
 
-original_image = np.array([[4,1,6,1,3],
-                           [3,2,7,7,2],
-                           [2,5,7,3,7],
-                           [1,4,7,1,3],
-                           [0,1,6,4,4]]).astype(dtype='Float32')
+OriginalImage = np.array([[4,1,6,1,3],
+                          [3,2,7,7,2],
+                          [2,5,7,3,7],
+                          [1,4,7,1,3],
+                          [0,1,6,4,4]]).astype(dtype='Float32')
 
 mean_filter = np.array([[1,1,1],
                         [1,1,1],
                         [1,1,1]]) / 9
 
 print("Original matrix:")
-print(original_image)
+print(OriginalImage)
 
-OriginalWithMeanFilter = scisig.convolve2d(original_image,mean_filter)
+OriginalWithMeanFilter = scisig.convolve2d(OriginalImage,mean_filter)
 
-print("\nConvolution with mean filter:")
+print("\nApplying mean filter:")
 print(OriginalWithMeanFilter)
 
 # default size for this function is 3X3
-OriginalWithMedianFilter = scisig.medfilt2d(original_image)
+OriginalWithMedianFilter = scisig.medfilt2d(OriginalImage)
 
 print("\nApplying median filter:")
 print(OriginalWithMedianFilter)
+
+# Sigma chosen randomly.
+OriginalWithGaussianFilter = scifilt.gaussian_filter(OriginalImage,sigma=0.5)
+
+print("\nApplying gaussian filter:")
+print(OriginalWithGaussianFilter)
+
+
