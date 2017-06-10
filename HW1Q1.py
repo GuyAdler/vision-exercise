a95c2b8a19da6cf86431aff29b551f2312730ebe
GuyAdler <krynnish.conspiracy@gmail.com>
2017-06-10 10:49:43 +0300

    removed unnecessary import statement

--- HW1Q1.py
+++ HW1Q1.py
@@ -4,7 +4,7 @@ import scipy.ndimage.filters as scifilt
 import matplotlib.pyplot as plt
 from skimage import feature, color, data
 from skimage.util import random_noise
-from scipy import ndimage
+
 
 OriginalImage = np.array([[4, 1, 6, 1, 3],
                           [3, 2, 7, 7, 2],
