f156f09d5f492e49e90a1b8e204cf52f48e98688
GuyAdler <krynnish.conspiracy@gmail.com>
2017-06-10 10:48:25 +0300

    changed HW1Q1 images to scikit-images data

--- HW1Q1.py
+++ HW1Q1.py
@@ -2,19 +2,19 @@ import numpy as np
 import scipy.signal as scisig
 import scipy.ndimage.filters as scifilt
 import matplotlib.pyplot as plt
-from skimage import feature, color
+from skimage import feature, color, data
 from skimage.util import random_noise
 from scipy import ndimage
 
-OriginalImage = np.array([[4,1,6,1,3],
-                          [3,2,7,7,2],
-                          [2,5,7,3,7],
-                          [1,4,7,1,3],
-                          [0,1,6,4,4]]).astype(dtype='Float32')
+OriginalImage = np.array([[4, 1, 6, 1, 3],
+                          [3, 2, 7, 7, 2],
+                          [2, 5, 7, 3, 7],
+                          [1, 4, 7, 1, 3],
+                          [0, 1, 6, 4, 4]]).astype(dtype='Float32')
 
-mean_filter = np.array([[1,1,1],
-                        [1,1,1],
-                        [1,1,1]]) / 9
+mean_filter = np.array([[1, 1, 1],
+                        [1, 1, 1],
+                        [1, 1, 1]]) / 9
 
 print("Original matrix:")
 print(OriginalImage)
@@ -35,7 +35,6 @@ print(OriginalWithMedianFilter)
 # mode='constant' is padding with zeros (the default constant value is 0)
 OriginalWithGaussianFilter = scifilt.gaussian_filter(OriginalImage, sigma=0.5, mode='constant')
 
-
 print("\nApplying gaussian filter:")
 print(OriginalWithGaussianFilter)
 
@@ -46,100 +45,100 @@ print(OriginalWithSobel)
 
 # This part with real photos
 EdgesThreshold = 0.3
-CameraMan = color.rgb2gray(ndimage.imread("HW1-Images\Q1\cameraman.jpg"))
-House = color.rgb2gray(ndimage.imread("HW1-Images\Q1\house.jpg"))
-Lena = color.rgb2gray(ndimage.imread("HW1-Images\Q1\lena.jpg"))
+CameraMan = color.rgb2gray(data.camera())
+Coffee = color.rgb2gray(data.coffee())
+Astronaut = color.rgb2gray(data.astronaut())
 
 fig1, axes1 = plt.subplots(3, 3)
 
 axes1[0, 0].imshow(CameraMan, cmap='gray')
 axes1[0, 0].axis('off')
 axes1[0, 0].set_title("Original camera man")
-axes1[0, 1].imshow(House, cmap='gray')
+axes1[0, 1].imshow(Coffee, cmap='gray')
 axes1[0, 1].axis('off')
-axes1[0, 1].set_title("Original house")
-axes1[0, 2].imshow(Lena, cmap='gray')
+axes1[0, 1].set_title("Original coffee")
+axes1[0, 2].imshow(Astronaut, cmap='gray')
 axes1[0, 2].axis('off')
-axes1[0, 2].set_title("Original Lena")
+axes1[0, 2].set_title("Original Astronaut")
 
 CameraManEdges = feature.canny(CameraMan, sigma=0, high_threshold=EdgesThreshold)
 CameraManGaussianEdges = feature.canny(CameraMan, sigma=2, high_threshold=EdgesThreshold)
-HouseEdges = feature.canny(House, sigma=0, high_threshold=EdgesThreshold)
-HouseGaussianEdges = feature.canny(House, sigma=2, high_threshold=EdgesThreshold)
-LenaEdges = feature.canny(Lena, sigma=0, high_threshold=EdgesThreshold)
-LenaGaussianEdges = feature.canny(Lena, sigma=2, high_threshold=EdgesThreshold)
+CoffeeEdges = feature.canny(Coffee, sigma=0, high_threshold=EdgesThreshold)
+CoffeeGaussianEdges = feature.canny(Coffee, sigma=2, high_threshold=EdgesThreshold)
+AstronautEdges = feature.canny(Astronaut, sigma=0, high_threshold=EdgesThreshold)
+AstronautGaussianEdges = feature.canny(Astronaut, sigma=2, high_threshold=EdgesThreshold)
 
 axes1[1, 0].imshow(CameraManEdges, cmap='gray')
 axes1[1, 0].axis('off')
 axes1[1, 0].set_title("Camera man edges")
-axes1[1, 1].imshow(HouseEdges, cmap='gray')
+axes1[1, 1].imshow(CoffeeEdges, cmap='gray')
 axes1[1, 1].axis('off')
-axes1[1, 1].set_title("House edges")
-axes1[1, 2].imshow(LenaEdges, cmap='gray')
+axes1[1, 1].set_title("Coffee edges")
+axes1[1, 2].imshow(AstronautEdges, cmap='gray')
 axes1[1, 2].axis('off')
-axes1[1, 2].set_title("Lena edges")
+axes1[1, 2].set_title("Astronaut edges")
 
 axes1[2, 0].imshow(CameraManGaussianEdges, cmap='gray')
 axes1[2, 0].axis('off')
 axes1[2, 0].set_title("Camera man edges after filter")
-axes1[2, 1].imshow(HouseGaussianEdges, cmap='gray')
+axes1[2, 1].imshow(CoffeeGaussianEdges, cmap='gray')
 axes1[2, 1].axis('off')
-axes1[2, 1].set_title("House edges after filter")
-axes1[2, 2].imshow(LenaGaussianEdges, cmap='gray')
+axes1[2, 1].set_title("Coffee edges after filter")
+axes1[2, 2].imshow(AstronautGaussianEdges, cmap='gray')
 axes1[2, 2].axis('off')
-axes1[2, 2].set_title("Lena edges after filter")
+axes1[2, 2].set_title("Astronaut edges after filter")
 
 # Adding noise and doing the same thing
 CameraManNoise = random_noise(CameraMan, mode='gaussian')
-HouseNoise = random_noise(House, mode='gaussian')
-LenaNoise = random_noise(Lena, mode='gaussian')
+CoffeeNoise = random_noise(Coffee, mode='gaussian')
+AstronautNoise = random_noise(Astronaut, mode='gaussian')
 
 fig2, axes2 = plt.subplots(3, 3)
 axes2[0, 0].imshow(CameraManNoise, cmap='gray')
 axes2[0, 0].axis('off')
 axes2[0, 0].set_title("Camera man with noise")
-axes2[0, 1].imshow(HouseNoise, cmap='gray') 
+axes2[0, 1].imshow(CoffeeNoise, cmap='gray')
 axes2[0, 1].axis('off')
-axes2[0, 1].set_title("House with noise")
-axes2[0, 2].imshow(LenaNoise, cmap='gray')
+axes2[0, 1].set_title("Coffee with noise")
+axes2[0, 2].imshow(AstronautNoise, cmap='gray')
 axes2[0, 2].axis('off')
-axes2[0, 2].set_title("Lena with noise")
+axes2[0, 2].set_title("Astronaut with noise")
 
 CameraManEdges = feature.canny(CameraManNoise, sigma=0, high_threshold=EdgesThreshold)
 CameraManGaussianEdges = feature.canny(CameraManNoise, sigma=2, high_threshold=EdgesThreshold)
-HouseEdges = feature.canny(HouseNoise, sigma=0, high_threshold=EdgesThreshold)
-HouseGaussianEdges = feature.canny(HouseNoise, sigma=2, high_threshold=EdgesThreshold)
-LenaEdges = feature.canny(LenaNoise, sigma=0, high_threshold=EdgesThreshold)
-LenaGaussianEdges = feature.canny(LenaNoise, sigma=2, high_threshold=EdgesThreshold)
+CoffeeEdges = feature.canny(CoffeeNoise, sigma=0, high_threshold=EdgesThreshold)
+CoffeeGaussianEdges = feature.canny(CoffeeNoise, sigma=2, high_threshold=EdgesThreshold)
+AstronautEdges = feature.canny(AstronautNoise, sigma=0, high_threshold=EdgesThreshold)
+AstronautGaussianEdges = feature.canny(AstronautNoise, sigma=2, high_threshold=EdgesThreshold)
 
 axes2[1, 0].imshow(CameraManEdges, cmap='gray')
 axes2[1, 0].axis('off')
 axes2[1, 0].set_title("Camera man edges")
-axes2[1, 1].imshow(HouseEdges, cmap='gray')
+axes2[1, 1].imshow(CoffeeEdges, cmap='gray')
 axes2[1, 1].axis('off')
-axes2[1, 1].set_title("House edges")
-axes2[1, 2].imshow(LenaEdges, cmap='gray')
+axes2[1, 1].set_title("Coffee edges")
+axes2[1, 2].imshow(AstronautEdges, cmap='gray')
 axes2[1, 2].axis('off')
-axes2[1, 2].set_title("Lena edges")
+axes2[1, 2].set_title("Astronaut edges")
 
 axes2[2, 0].imshow(CameraManGaussianEdges, cmap='gray')
 axes2[2, 0].axis('off')
 axes2[2, 0].set_title("Camera man edges after filter")
-axes2[2, 1].imshow(HouseGaussianEdges, cmap='gray')
+axes2[2, 1].imshow(CoffeeGaussianEdges, cmap='gray')
 axes2[2, 1].axis('off')
-axes2[2, 1].set_title("House edges after filter")
-axes2[2, 2].imshow(LenaGaussianEdges, cmap='gray')
+axes2[2, 1].set_title("Coffee edges after filter")
+axes2[2, 2].imshow(AstronautGaussianEdges, cmap='gray')
 axes2[2, 2].axis('off')
-axes2[2, 2].set_title("Lena edges after filter")
+axes2[2, 2].set_title("Astronaut edges after filter")
 
 fig3, axes3 = plt.subplots(2, 3)
 
 CameraManFiltered075 = scifilt.gaussian_filter(CameraMan, sigma=0.75)
 CameraManFiltered250 = scifilt.gaussian_filter(CameraMan, sigma=2.5)
-HouseFiltered075 = scifilt.gaussian_filter(House, sigma=0.75)
-HouseFiltered250 = scifilt.gaussian_filter(House, sigma=2.5)
-LenaFiltered075 = scifilt.gaussian_filter(Lena, sigma=0.75)
-LenaFiltered250 = scifilt.gaussian_filter(Lena, sigma=2.5)
+CoffeeFiltered075 = scifilt.gaussian_filter(Coffee, sigma=0.75)
+CoffeeFiltered250 = scifilt.gaussian_filter(Coffee, sigma=2.5)
+AstronautFiltered075 = scifilt.gaussian_filter(Astronaut, sigma=0.75)
+AstronautFiltered250 = scifilt.gaussian_filter(Astronaut, sigma=2.5)
 
 axes3[0, 0].imshow(2*CameraMan - CameraManFiltered075, cmap='gray')
 axes3[0, 0].axis('off')
@@ -147,18 +146,18 @@ axes3[0, 0].set_title("Camera man, unsharped masking - sigma = 0.75")
 axes3[1, 0].imshow(2*CameraMan - CameraManFiltered250, cmap='gray')
 axes3[1, 0].axis('off')
 axes3[1, 0].set_title("Camera man, unsharped masking - sigma = 2.5")
-axes3[0, 1].imshow(2*House - HouseFiltered075, cmap='gray')
+axes3[0, 1].imshow(2 * Coffee - CoffeeFiltered075, cmap='gray')
 axes3[0, 1].axis('off')
-axes3[0, 1].set_title("House, unsharped masking - sigma = 0.75")
-axes3[1, 1].imshow(2*House - HouseFiltered250, cmap='gray')
+axes3[0, 1].set_title("Coffee, unsharped masking - sigma = 0.75")
+axes3[1, 1].imshow(2 * Coffee - CoffeeFiltered250, cmap='gray')
 axes3[1, 1].axis('off')
-axes3[1, 1].set_title("House, unsharped masking - sigma = 2.5")
-axes3[0, 2].imshow(2*Lena - LenaFiltered075, cmap='gray')
+axes3[1, 1].set_title("Coffee, unsharped masking - sigma = 2.5")
+axes3[0, 2].imshow(2 * Astronaut - AstronautFiltered075, cmap='gray')
 axes3[0, 2].axis('off')
-axes3[0, 2].set_title("Lena, unsharped masking - sigma = 0.75")
-axes3[1, 2].imshow(2*Lena - LenaFiltered250, cmap='gray')
+axes3[0, 2].set_title("Astronaut, unsharped masking - sigma = 0.75")
+axes3[1, 2].imshow(2 * Astronaut - AstronautFiltered250, cmap='gray')
 axes3[1, 2].axis('off')
-axes3[1, 2].set_title("Lena, unsharped masking - sigma = 2.5")
+axes3[1, 2].set_title("Astronaut, unsharped masking - sigma = 2.5")
 
 
 plt.show()
